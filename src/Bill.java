a1065d685bfc29b7c661eb164ec92e156c66ec21
Jose Augusto Scherer <jose.augusto.github@gmail.com>
2025-09-03 21:21:16 -0300

    Firts commit

---
+++ src/Bill.java
@@ -0,0 +1,57 @@
+public class Bill {
+
+    private char gender;
+    private int beer;
+    private int barbecue;
+    private int softDrink;
+
+    public double cover() {
+        if ( feeding() > 30.00 ) {
+            return 0.0;
+        } else {
+            return 4.00;
+        }
+    }
+
+    public double feeding() {
+        return (beer * 5.00) + (barbecue * 7.00) + (softDrink * 3.00);
+    }
+
+    public double ticket() {
+        if (gender == 'F') {
+            return 8.00;
+        } else {
+            return 10.0;
+        }
+    }
+
+    public double total() {
+        return cover() + feeding() + ticket();
+    }
+
+    public Bill() {
+    }
+
+    public Bill(char gender, int beer, int barbecue, int softDrink) {
+        this.gender = gender;
+        this.beer = beer;
+        this.barbecue = barbecue;
+        this.softDrink = softDrink;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getBeer() {
+        return beer;
+    }
+
+    public int getBarbecue() {
+        return barbecue;
+    }
+
+    public int getSoftDrink() {
+        return softDrink;
+    }
+}
